04346c11b3f6ce09a77254a66542559d0ac39148
James Grodzicki <jamespgrodzicki@gmail.com>
2019-10-10 21:10:57 -0500

    connected html pages

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+const friendsArray = [
+    {
+        name: 'john',
+        age: 35,
+        phoneNumber: 333-333-3333,
+        q1: 3,
+        q2: 3,
+        q3: 3,
+        q4: 3,
+        q5: 3,
+        q6: 3,
+        q7: 3,
+        q8: 3,
+        q9: 3,
+        q10: 3
+    }
+];
+
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 require('./app/routing/apiRoutes')(app);
-require('./app/routing/htmlRoutes')(app);
+// require('./app/routing/htmlRoutes')(app);
 
 app.listen(PORT, function(){
     console.log('Server: http://localhost:' + PORT);
