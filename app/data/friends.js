6925c6f6e8a9e8cace08a7cd915b3a0e8604aa35
James Grodzicki <jamespgrodzicki@gmail.com>
2019-10-15 20:43:20 -0500

    finished questionnaire

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,17 +3,15 @@ const friendsArray = [
         name: 'john',
         age: '35',
         phoneNumber: '333-333-3333',
-        q1: 3,
-        q2: 3,
-        q3: 3,
-        q4: 3,
-        q5: 3,
-        q6: 3,
-        q7: 3,
-        q8: 3,
-        q9: 3,
-        q10: 3
+        scores: ['3', '3', '3', '3', '3']
     }
 ];
 
-module.exports = friendsArray;
+function compatibility(userArr, existingArr){
+    
+}
+
+module.exports = {
+    friendsArray: friendsArray,
+    compatibility: compatibility
+};
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,16 @@
 const friendsData = require('../data/friends.js');
+const friendsArray = friendsData.friendsArray;
+const compatibility = friendsData.compatibility;
 
 module.exports = function(app){
     app.get('/api/friends', function(req, res){
-        res.json(friendsData);
+        res.json(friendsArray);
     });
 
     app.post('/api/friends', function(req, res){
-        friendsData.push(req.body);
+        compatibility(req.body.scores, friendsArray);
+        friendsArray.push(req.body);
         console.log(req.body);
-        res.json(friendsData);
+        res.json(friendsArray);
     });
 };
